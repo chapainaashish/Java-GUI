e24917c310e33251e6277b6bea42d19bf90924ba
Aashish Chapain <ashishchapain86@gmail.com>
2023-08-12 08:53:13 +0545

    added jdbc example

--- Theory/JDBCout.java
+++ Theory/JDBCout.java
@@ -12,13 +12,29 @@ public class JDBCout {
             String sql = "SELECT id, user FROM User";
             ResultSet resultSet = stmt.executeQuery(sql);
 
+            // Create a multidimensional array to store the data
+            int rowCount = 0;
+            while (resultSet.next()) {
+                rowCount++;
+            }
 
+            resultSet.beforeFirst(); // Move the cursor back to the beginning
+
+            String[][] data = new String[rowCount][2]; // Assuming 2 columns: id and user
+
+
+
+            int index = 0;
             while (resultSet.next()) {
                 String user = resultSet.getString("user");
                 int id = resultSet.getInt("id");
 
                 System.out.println("User: " + user);
                 System.out.println("Id: " + id);
+
+                data[index][0] = Integer.toString(id);
+                data[index][1] = user;
+                index++;
             }
 
 
