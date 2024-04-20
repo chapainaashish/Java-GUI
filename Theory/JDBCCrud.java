fd3a2025b76f64bfdef77adab022d72e83cbaab6
Aashish Chapain <ashishchapain86@gmail.com>
2024-04-20 09:39:47 +0545

    refactored code

--- Theory/JDBCCrud.java
+++ Theory/JDBCCrud.java
@@ -5,7 +5,7 @@ import java.sql.*;
 /*
 executeQuery() ----- SELECT [return resultset in ResultSet data type]
 executeUpdate() ------ UPDATE, DELETE, CREATE         JRadioButton radioButton1 = new JRadioButton("Radio Button 1");
-[return num of row affected in int]
+[return num of row affected in int] 
  */
 public class JDBCCrud {
     JDBCCrud(){
